16ac2b696fbbb9b93ebaf6aad12c52161d5617b0
hhh146578 <hhh146578@gmail.com>
2017-07-04 15:25:55 +0800

    add google marker

--- src/components/initMap.js
+++ src/components/initMap.js
@@ -1,13 +1,24 @@
 
 function initialize() {
+  var myLatLng = {lat: 22.6, lng: 120.34};
   var map = new google.maps.Map(
     document.getElementById("map_canvas"), {
-      center: new google.maps.LatLng(22.6, 120.34),
-      zoom: 13,
+      center: myLatLng,
+      zoom: 15,
       mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
-
+  });
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map
+  });
 }
+
+// var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+// var beachMarker = new google.maps.Marker({
+//   position: {lat: -33.890, lng: 151.274},
+//   map: map,
+//   icon: image
+// });
 
 google.maps.event.addDomListener(window, "load", initialize);
 
